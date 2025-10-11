76251e75fdf38a8a5964587327e5313f961ef784
dishb <code.dishb@gmail.com>
2025-10-11 11:39:10 -0700

    fix: links on events page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,26 @@
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <div className="relative h-150 p-6 bg-gradient-to-b from-amber-500 to-purple-900">
+        <div className="relative h-full rounded-xl overflow-hidden bg-[url(/photos/3.jpg)] bg-cover bg-[0%_80%]">
+          <div className="absolute inset-0 bg-gradient-to-t from-[#2A1044]/40 to-[#2A1044]/90" />
+          <div className="relative z-10 h-full w-full flex justify-evenly items-center">
+            <div className="max-w-[40%]">
+              <h1 className="text-6xl font-bold">About</h1>
+
+              <h2 className="mt-12 text-3xl text-purple-300">
+                What is TSA and why you should get involved.
+              </h2>
+            </div>
+
+            <div className="min-w-[40%]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="px-10">
+        
+      </div>
+    </>
+  );
 }
